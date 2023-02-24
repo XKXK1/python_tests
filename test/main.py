ba1ac82724b5b39c38908056c087f0e1058be938
derya <derya.uyargil@hotmail.de>
2023-02-24 20:16:34 +0100

    formatting and format

--- test/main.py
+++ test/main.py
@@ -1,6 +1,6 @@
 import sys
 from data.Plant import *
-import datetime 
+import datetime
 from service.plantService import *
 
 
@@ -10,9 +10,10 @@ def main():
         print(plant)
         waterPlant
         print(plant)
-        
+
     except ValueError as ve:
         return str(ve)
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- test/service/plantService.py
+++ test/service/plantService.py
@@ -1,6 +1,7 @@
 import datetime
 from data.Plant import Plant
 
+
 def waterPlant(plant: Plant):
-  plant.lastWatered = datetime.datetime.now()
-  plant.needsWater = False
+    plant.lastWatered = datetime.datetime.now()
+    plant.needsWater = False
