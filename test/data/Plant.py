eae513c643a64fcfba3d5fbf76b443a6cde0a0ce
derya <derya.uyargil@hotmail.de>
2023-02-24 20:16:18 +0100

    add formatter and formatting

--- test/data/Plant.py
+++ test/data/Plant.py
@@ -2,19 +2,16 @@ from dataclasses import dataclass
 from enum import Enum
 import datetime
 
+
 class WaterType(Enum):
     LOW = 1
     MEDIUM = 2
     HIGH = 3
 
+
 @dataclass
 class Plant:
-  name: str
-  waterType: WaterType
-  lastWatered: datetime
-  needsWater: bool = True
-  
-
-  
-  
-
+    name: str
+    waterType: WaterType
+    lastWatered: datetime
+    needsWater: bool = True
