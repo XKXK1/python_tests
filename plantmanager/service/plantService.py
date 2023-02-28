377703b196dc65196f0abfb5556a67a86397290a
XKXK1 <deryauyargil@hotmail.de>
2023-02-28 21:37:30 +0100

    add check if plant needs water and add test

--- plantmanager/service/plantService.py
+++ plantmanager/service/plantService.py
@@ -1,8 +1,23 @@
-import datetime
-from data.Plant import Plant
+from datetime import datetime, timedelta
+from data.Plant import *
 
+WATER_TYPE_LOW_DAY_LIMIT = 1
+WATER_TYPE_MID_DAY_LIMIT = 2
+WATER_TYPE_HIGH_DAY_LIMIT = 3
 
 class PlantService:
     def waterPlant(self, plant: Plant):
         plant.lastWatered = datetime.datetime.now()
         plant.needsWater = False
+    
+    def plant_needs_water(self, plant: Plant): 
+        time = datetime.datetime.now()
+        match plant.waterType:
+            case WaterType.MEDIUM:
+                return plant.lastWatered < time - timedelta(days=WATER_TYPE_LOW_DAY_LIMIT)
+            case WaterType.MEDIUM:
+                return plant.lastWatered < time - timedelta(days=WATER_TYPE_MID_DAY_LIMIT)
+            case WaterType.HIGH:
+                return plant.lastWatered < time - timedelta(days=WATER_TYPE_HIGH_DAY_LIMIT)
+            case default:
+                return "something"
--- plantmanager/test/plant_manager_test.py
+++ plantmanager/test/plant_manager_test.py
@@ -1,7 +1,7 @@
 import unittest
+from service.plantService import *
 from data.Plant import *
 import datetime
-from service.plantService import *
 
 
 class plant_manager_test(unittest.TestCase):
@@ -19,6 +19,19 @@ class plant_manager_test(unittest.TestCase):
 
         # then
         self.assertFalse(testPlant.needsWater)
+        
+    def test_plant_needs_water(self):
+        #given
+        plantService = PlantService()
+        date = datetime.datetime.now() - timedelta(days=10)
+
+        testPlant = Plant(1,"Monstera", WaterType.MEDIUM,
+                          date)        
+        #when
+        needsWater = plantService.plant_needs_water(testPlant)
+        
+        #then
+        self.assertTrue(needsWater)
 
 
 if __name__ == '__main__':
