d9ec0bc92ba3e39dd31713f1dc29737555d36494
XKXK1 <deryauyargil@hotmail.de>
2023-02-28 21:37:47 +0100

    commit

--- plantmanager/repository/plant_storage.py
+++ plantmanager/repository/plant_storage.py
@@ -1,16 +1,17 @@
 from data.Plant import *
 import datetime
 
+plants = [
+    Plant(1, "Monstera", WaterType.HIGH, datetime.datetime.now(),  False),
+    Plant(2, "Palm Tree", WaterType.MEDIUM,
+          datetime.datetime.now(), False),
+    Plant(3, "Dracae", WaterType.LOW, datetime.datetime.now(), True),
+    Plant(4, "Tree", WaterType.LOW, datetime.datetime.now(), False),
+    Plant(5, "Tulip", WaterType.HIGH, datetime.datetime.now(), False)
+]
+
 
 class PlantStorage:
-    plants = [
-        Plant(1, "Monstera", WaterType.HIGH, datetime.datetime.now(),  False),
-        Plant(2, "Palm Tree", WaterType.MEDIUM,
-              datetime.datetime.now(), False),
-        Plant(3, "Dracae", WaterType.LOW, datetime.datetime.now(), True),
-        Plant(4, "Tree", WaterType.LOW, datetime.datetime.now(), False),
-        Plant(5, "Tulip", WaterType.HIGH, datetime.datetime.now(), False)
-    ]
 
     def getAllPlants(self) -> list[Plant]:
         return self.plants
@@ -19,10 +20,10 @@ class PlantStorage:
         '''returns one plant by id'''
         # Using filter() Method
         plant = next(filter(lambda plant: plant.id == id, self.plants))
-        
+
         if plant is None:
-            raise ValueError
+            raise Exception("Date provided can't be in the past")
         return plant
-    
+
         # List Comprehension
-        #filteredPlants = [plant for plant in self.plants if plant.id == id]
+        # filteredPlants = [plant for plant in self.plants if plant.id == id]
